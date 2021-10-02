309584b0f707bf2c584f5ec31f9f817eb832174e
YeJi Song <89468282+foresight128@users.noreply.github.com>
2021-10-02 14:16:12 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # carrot_game
+
+url - https://foresight128.github.io/carrot_game/
+
+Date - 2021/10/02
+
+Tool & Skill
+- html5
+- css3
+- javascript
+- VisualStudioCode
+
+* * *
+
+지정된 시간 안에 모든 당근을 뽑아내면 성공하는 게임
+
---
+++ src/main.js
@@ -0,0 +1,41 @@
+'use strict';
+import PopUp from './popup.js';
+import {Gamebuilder, Reason } from './game.js';
+import * as sound from './sound.js';
+
+const gameFinishBanner = new PopUp();
+const game = new Gamebuilder()
+.withGameDuration(5)
+.withCarrotCount(5)
+.withBugCound(5)
+.build();
+
+game.setGameStopListener(reason => {
+  let message;
+  switch(reason){
+    case Reason.cancel:
+      message = 'REPLAY❓';
+      sound.playAlert();
+      break;
+    case Reason.win:
+      message = 'YOU WON🎉';
+      sound.playWin();
+      break;
+    case Reason.lose:
+      message = 'YOU LOST💩';
+      sound.playBug();
+      break;
+    default:
+      throw new Error('not valid reason');
+  };
+  gameFinishBanner.showWithText(message);
+});
+
+gameFinishBanner.setClickListener(() => {
+  game.start();
+});
+
+
+
+
+
